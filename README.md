d55d522da4be6b83599e34d128cabe29edb300c2
Douglas Dias Leal <douglas_san@hotmail.com>
2024-12-13 04:27:11 +0900

    feat: create hello world app

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # NLW18-React-Native
+
+Create the project as Navigation (TypeScript):
+```sh
+npx create-expo-app@latest --template
+```
+
+Start the project:
+```sh
+npx expo start
+```
+
+- [Figma](https://www.figma.com/design/XYi54I0Mkj6gwjBRS07JRA/NLW-Pocket-Mobile-•-Nearby?node-id=0-1&node-type=canvas&t=mbpL6WwhnOQWGqQQ-0)
---
+++ mobile/src/app/index.tsx
@@ -0,0 +1,16 @@
+import { View, Text } from "react-native"
+
+export default function Index() {
+  return (
+    <View 
+      style={{ 
+        backgroundColor: "white", 
+        flex: 1, 
+        justifyContent: "center",
+        alignItems: "center"
+      }}
+    >
+      <Text style={{ fontSize: 22 }}>Hello React Native</Text>
+    </View>
+  )
+}
